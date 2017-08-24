3f2f9761b317d1e64b82353fbec8c24e9ec92c41
Daniel Garcia and Dawson Mortenson <daniel+dawson@>
2017-08-24 16:37:04 -0700

    finish the js file of tic tac toe

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,48 @@
 function Player(name, mark) {
   this.name = name;
   this.mark = mark;
+  this.score = 0;
 }
 Player.prototype.mark = function(){
   return this.mark;
 }
-function Space(mark) {
-  this.mark = mark;
-}
-// PASS IN THE player.mark function it will return their mark
-Space.prototype.mark = function(playerMark){
-  // Mark the space on the board
-  return playerMark;
-}
+// function Space(mark) {
+//   this.mark = mark;
+// }
+// // PASS IN THE player.mark function it will return their mark
+// // Space.prototype.mark = function(playerMark){
+//   // Mark the space on the board
+//   // return playerMark;
+// }
 function Board() {
-  this.boardmatrix = [[ 0, 1, 2 ], [ 0, 1, 2 ], [ 0, 1, 2 ]]
-}
-function Game(board, player1, player2) {
+  this.boardWin = [7, 56, 73, 84, 448, 146, 273, 292];
+  this.board = [1, 2, 4, 8, 16, 32 , 64, 128, 256];
+};
+function Game(board, player1, player2, spot) {
   this.board = board;
-  this.player1 = player1;
-  this.player2 = player2;
+  this.players = [player1, player2];
   this.turnTracker = 1; //Tracks turn using even or odds
-}
+};
+Game.prototype.turn = function (spot) {
+  if (this.turnTracker % 2 != 0){
+    player = this.players[0];
+  } else {
+    player = this.players[1];
+  }
+  player.score += this.board.board[spot - 1];
+  this.turnTracker += 1;
+
+  this.win(player);
+
+  return player.mark;
+};
+Game.prototype.win = function (currentPlayer) {
+  for(var i = 0; i < this.board.boardWin.length; i++){
+    if((this.board.boardWin[i] & currentPlayer.score) === this.board.boardWin[i]){
+      alert(currentPlayer.mark + " has won!");
+    }
+  }
+};
 
 
 $(document).ready(function() {
@@ -31,14 +52,14 @@ $(document).ready(function() {
   var gameBoard = new Board();
   var theGame = new Game(gameBoard, player1, player2)
 
-  $(".spots").click(function() {
-    if(theGame.turnTracker % 2 != 0){
-      $(this).find(".mark-text").text("X");
+  $(".spots").click(function(event) {
+    if ($(this).find(".mark-text").text() === ""){
+      $(this).find(".mark-text").text(theGame.turn(event.target.id));
     } else {
-      $(this).find(".mark-text").text("O");
+      alert("That space has already been marked!");
     }
     // $(this).val("player1");
     // console.log($(this).val());
-    theGame.turnTracker += 1;
+
   });
 });
